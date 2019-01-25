5be1c463c1eebb65ae77f74bc67114b170469ff2
zhang <zhang@tedu.cn>
2019-01-25 15:28:57 +0800

    15:20

---
+++ get_web.py
@@ -0,0 +1,39 @@
+import re
+import os
+import urllib.error
+from urllib import request
+
+def download(url, fname):
+    html = request.urlopen(url)
+    with open(fname, 'wb') as fobj:
+        while True:
+            data = html.read(1024)
+            if not data:
+                break
+            fobj.write(data)
+
+def get_url(fname, patt, encoding=None):
+    url_list = []
+    cpatt = re.compile(patt)
+    with open(fname, encoding=encoding) as fobj:
+        for line in fobj:
+            for m in cpatt.finditer(line):    # [匹配对象，匹配对象]
+                url_list.append(m.group())
+    return url_list
+
+if __name__ == '__main__':
+    net_url = 'http://www.163.com/'
+    fname = '/tmp/163.html'
+    url_patt = '(http|https)://[-\w./]+\.(png|jpg|jpeg|gif)'
+    download(net_url, fname)
+    url_list = get_url(fname, url_patt, encoding='gbk')
+    folder = '/tmp/wangyi/'   # 保存图片的本地目录
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+    for url in url_list:
+        fname = url.split('/')[-1]    # 取出网址中的文件名
+        fname = os.path.join(folder, fname)   # 拼接本地文件绝对路径
+        try:
+            download(url, fname)
+        except urllib.error.HTTPError:
+            print(url)
